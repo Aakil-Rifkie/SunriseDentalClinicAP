b7648402ef3f1ad01baf8cfef0bf52ad114276ee
Aakil Rifkie <aakil.rifkie@gmail.com>
2026-08-26 22:22:46 +0530

    Added database connection code and files to views

--- src/sunrisedentalclinic/database/DBConnection.java
+++ src/sunrisedentalclinic/database/DBConnection.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sunrisedentalclinic.database;
 
-/**
- *
- * @author user
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
-    
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sunrisedentalclinic_DB?useSSL=false", "root", "");
+            System.out.println("Database Connected Successfully!");
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e);
+        }
+        return con;
+    }
 }
---
+++ src/sunrisedentalclinic/database/Testconnection.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sunrisedentalclinic.database;
+
+/**
+ *
+ * @author user
+ */
+public class Testconnection {
+    public static void main(String args[]){
+        DBConnection.getConnection();
+    }
+}
