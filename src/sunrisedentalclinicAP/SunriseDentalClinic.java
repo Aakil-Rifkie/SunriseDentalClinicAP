f196e51abde64c2c48499747c4ef1d76dca752d1
Aakil Rifkie <aakil.rifkie@gmail.com>
2026-09-01 19:51:00 +0530

    Created staff handler class, and HTTP server in main

--- src/sunrisedentalclinicAP/SunriseDentalClinic.java
+++ src/sunrisedentalclinicAP/SunriseDentalClinic.java
@@ -4,17 +4,43 @@
  */
 package sunrisedentalclinicAP;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 /**
  *
  * @author user
  */
 public class SunriseDentalClinic {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+       try {
+           HttpServer server = HttpServer.create(new InetSocketAddress(5000), 0);
+           server.createContext("/", new MyHandler());
+           server.setExecutor(null);
+           server.start();
+           
+           System.out.println("Server is running port 5000");
+       } 
+       catch(IOException e) {
+           System.out.println("Error starting the erorr" + e.getMessage());
+       }
     }
     
+    static class MyHandler implements HttpHandler{
+        @Override
+        public void handle(HttpExchange exchange) throws IOException
+        {
+            String response = "Hello this is a simple HTTP server";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 }
