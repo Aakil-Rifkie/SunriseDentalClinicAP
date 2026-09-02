cbff77c081a80973df46ea3e6e21ca8a633058d7
Aakil Rifkie <aakil.rifkie@gmail.com>
2026-09-02 11:57:13 +0530

    Created StaffController to connect UI and api client

---
+++ src/sunrisedentalclinic/controllers/StaffController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sunrisedentalclinic.controllers;
+import sunrisedentalclinic.controllers.ApiClient;
+/**
+ *
+ * @author user
+ */
+public class StaffController {
+    private ApiClient apiClient = new ApiClient();
+    
+    public boolean handleLogin(String username, String password){
+        String payload = username + "," + password;
+        
+        try {
+            String response = apiClient.post("/login", payload);
+            
+            return Boolean.parseBoolean(response.trim());
+        }
+        catch(Exception e){
+            System.out.println("Network or Login Error: " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean handleRegistration(String username, String password, String role){
+        String payload = username + "," + password + "," + role;
+        
+        try {
+            String response = apiClient.post("/register", payload);
+            return Boolean.parseBoolean(response.trim());
+        }
+        catch(Exception e) {
+            System.out.println("Network or Registration error: " + e.getMessage());
+            return false;
+        }
+    }
+}
