9a79981d9f4181b35f09e16b05d30fccfb469a57
Aakil Rifkie <aakil.rifkie@gmail.com>
2026-09-03 16:51:41 +0530

    Created Receipt model and fetch and save billing details to the billingDAO

---
+++ src/sunrisedentalclinic/dao/BillingDAO.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sunrisedentalclinic.dao;
+import sunrisedentalclinic.database.DBConnection;
+import sunrisedentalclinic.models.Receipt;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author user
+ */
+public class BillingDAO {
+    public static final double CONSULTATION_FEE = 1500.00;
+    
+    public Receipt getBillingDetails(int apptId){
+        String sql = "SELECT a.appt_id, p.name AS patient_name, a.dentist_name, t.name AS treatment_name, t.cost AS treatment_cost " +
+                "FROM appointments a " +
+                "INNER JOIN patients p ON a.patient_id = p.patient_id " +
+                "INNER JOIN treatments t ON a.treatment_type = t.name " +
+                "WHERE a.appt_id = ?";
+        
+        try (Connection con = DBConnection.getConnection();
+                PreparedStatement pst = con.prepareStatement(sql)){
+            pst.setInt(1, apptId);
+            try(Resultset rs = pst.executeQuery()){
+                if(rs.next()){
+                    return new Receipt(
+                    rs.getInt("appt_id"),
+                    rs.getString("patient_name"),
+                    rs.getString("dentist_name"),
+                    rs.getString("treatment_name"),
+                    rs.getDouble("treatment_cost"),
+                    CONSULTATION_FEE
+                    );
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Error fetching billing details: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    public boolean saveReceipt(Receipt receipt){
+        String sql = "INSERT INTO receipts (appt_id, total_amount) VALUES (?, ?)";
+        boolean isSuccess = false;
+        
+        try (Connection con = DBConnection.getConnection();
+        PreparedStatement pst = con.prepareStatement(sql)){
+            pst.setInt(1, receipt.getAppointmentID());
+            pst.setDouble(2, receipt.getTotalAmount());
+            
+            int rowsAffected = pst.executeUpdate();
+            if (rowsAffected > 0){
+                isSuccess = true;
+            }
+        } catch (Exception e){
+            System.out.println("Error saving receipts" +  e.getMessage());
+        }
+        return isSuccess;
+    }
+}
---
+++ src/sunrisedentalclinic/models/Receipt.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sunrisedentalclinic.models;
+
+/**
+ *
+ * @author user
+ */
+public class Receipt {
+    private int receiptID;
+    private int appointmentID;
+    private String patientName;
+    private String dentistName;
+    private String treatmentName;
+    private double treatmentCost;
+    private double consultationFee;
+    private double totalAmount;
+    private String issueDate;
+
+    public Receipt(int appointmentID, String patientName, String dentistName, String treatmentName, double treatmentCost, double consultationFee) {
+        this.appointmentID = appointmentID;
+        this.patientName = patientName;
+        this.dentistName = dentistName;
+        this.treatmentName = treatmentName;
+        this.treatmentCost = treatmentCost;
+        this.consultationFee = consultationFee;
+        this.totalAmount = treatmentCost + consultationFee;
+    }
+
+    public int getReceiptID() {
+        return receiptID;
+    }
+
+    public int getAppointmentID() {
+        return appointmentID;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getDentistName() {
+        return dentistName;
+    }
+
+    public String getTreatmentName() {
+        return treatmentName;
+    }
+
+    public double getTreatmentCost() {
+        return treatmentCost;
+    }
+
+    public double getConsultationFee() {
+        return consultationFee;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setReceiptID(int receiptID) {
+        this.receiptID = receiptID;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }  
+ }
